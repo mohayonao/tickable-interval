cf42380d565580fb58d785ead207e37cf2780bc6
mohayonao <mohayonao@gmail.com>
2015-01-23 10:57:37 +0900

    remove the inline source-map from the build file

--- test/tickable-interval.js
+++ test/tickable-interval.js
@@ -1,8 +1,5 @@
 "use strict";
 
-import support from "source-map-support";
-support.install();
-
 import assert from "power-assert";
 import {TickableInterval} from "../lib/tickable-interval";
 
@@ -15,78 +12,78 @@ describe("TickableInterval", ()=> {
 
     // setInterval
     interval.set(() => { fired += 1 }, 1000);
-    assert(fired === 0);
-    assert(interval.remain === 1000);
+    assert(fired === 0, "00:00.000");
+    assert(interval.remain === 1000, "00:00.000");
 
     // 00:00.000 -> 00:01.000
     interval.tick(250);
-    assert(fired === 0);
-    assert(interval.remain === 750);
+    assert(fired === 0, "00:00.250");
+    assert(interval.remain === 750, "00:00.250");
 
     interval.tick(250);
-    assert(fired === 0);
-    assert(interval.remain === 500);
+    assert(fired === 0, "00:00.500");
+    assert(interval.remain === 500, "00:00.500");
 
     interval.tick(250);
-    assert(fired === 0);
-    assert(interval.remain === 250);
+    assert(fired === 0, "00:00.750");
+    assert(interval.remain === 250, "00:00.750");
 
     interval.tick(250);
-    assert(fired === 1);
-    assert(interval.remain === 1000);
+    assert(fired === 1, "00:01.000");
+    assert(interval.remain === 1000, "00:01.000");
 
     // 00:01.000 -> 00:02.000
     interval.tick(250);
-    assert(fired === 1);
-    assert(interval.remain === 750);
+    assert(fired === 1, "00:01.250");
+    assert(interval.remain === 750, "00:01.250");
 
     interval.tick(250);
-    assert(fired === 1);
-    assert(interval.remain === 500);
+    assert(fired === 1, "00:01.500");
+    assert(interval.remain === 500, "00:01.500");
 
     interval.tick(250);
-    assert(fired === 1);
-    assert(interval.remain === 250);
+    assert(fired === 1, "00:01.750");
+    assert(interval.remain === 250, "00:01.750");
 
     interval.tick(250);
-    assert(fired === 2);
-    assert(interval.remain === 1000);
+    assert(fired === 2, "00:02.000");
+    assert(interval.remain === 1000, "00:02.000");
 
     // clearInterval
     interval.clear();
 
     // 00:02.000 -> 00:03.000
     interval.tick(250);
-    assert(fired === 2);
-    assert(interval.remain === Infinity);
+    assert(fired === 2, "00:02.250");
+    assert(interval.remain === Infinity, "00:02.250");
 
     interval.tick(250);
-    assert(fired === 2);
-    assert(interval.remain === Infinity);
+    assert(fired === 2, "00:02.500");
+    assert(interval.remain === Infinity, "00:02.500");
 
     interval.tick(250);
-    assert(fired === 2);
-    assert(interval.remain === Infinity);
+    assert(fired === 2, "00:02.750");
+    assert(interval.remain === Infinity, "00:02.750");
 
     interval.tick(250);
-    assert(fired === 2);
-    assert(interval.remain === Infinity);
+    assert(fired === 2, "00:03.000");
+    assert(interval.remain === Infinity, "00:03.000");
 
     // 00:03.000 -> 00:04.000
     interval.tick(250);
-    assert(fired === 2);
-    assert(interval.remain === Infinity);
+    assert(fired === 2, "00:03.250");
+    assert(interval.remain === Infinity, "00:03.250");
 
     interval.tick(250);
-    assert(fired === 2);
-    assert(interval.remain === Infinity);
+    assert(fired === 2, "00:03.500");
+    assert(interval.remain === Infinity, "00:03.500");
 
     interval.tick(250);
-    assert(fired === 2);
-    assert(interval.remain === Infinity);
+    assert(fired === 2, "00:03.750");
+    assert(interval.remain === Infinity, "00:03.750");
 
     interval.tick(250);
-    assert(fired === 2);
-    assert(interval.remain === Infinity);
+    assert(fired === 2, "00:04.000");
+    assert(interval.remain === Infinity, "00:04.000");
   });
 });
