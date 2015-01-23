9f3b17899c8b9a175dc8cb08355850585b421ec2
mohayonao <mohayonao@gmail.com>
2015-01-23 09:42:54 +0900

    change the interface accessibility

--- src/tickable-interval.js
+++ src/tickable-interval.js
@@ -1,51 +1,54 @@
 "use strict";
 
 /**
- * The manual ticking `setInterval`
- * @class TickableInterval
+ * The manual ticking `setInterval` / `clearInterval`
+ * @class
+ * @property {function} callback
+ * @property {number} interval
+ * @property {number} remain
  */
 export class TickableInterval {
   constructor() {
-    if (!(this instanceof TickableInterval)) {
-      return new TickableInterval();
-    }
+    this.callback = null;
+    this.interval = Infinity;
+    this.remain = Infinity;
+  }
 
-    var _callback = null;
-    var _interval = Infinity;
-    var _remain = Infinity;
+  /**
+   * setInterval
+   * @param {function} callback
+   * @param {number} interval
+   * @public
+   */
+  set(callback, interval) {
+    this.callback = callback;
+    this.interval = Math.max(1, +interval|0);
+    this.remain = this.interval;
+  }
 
-    /**
-     * @api public
-     * @param {function} callback
-     * @param {number} interval
-     */
-    this.set = (callback, interval)=> {
-      _callback = callback;
-      _interval = Math.max(1, +interval);
-      _remain = _interval;
-    };
+  /**
+   * clearInterval
+   * @public
+   */
+  clear() {
+    this.callback = null;
+    this.interval = Infinity;
+    this.remain = Infinity;
+  }
 
-    /**
-     * @api public
-     */
-    this.clear = ()=> {
-      _callback = null;
-      _interval = Infinity;
-      _remain = Infinity;
-    };
-
-    /**
-     * @api public
-     * @param {number} tick
-     */
-    this.tick = (tick)=> {
-      if (typeof _callback === "function") {
-        _remain -= tick;
-        while (_remain <= 0) {
-          _callback();
-          _remain += _interval;
-        }
+  /**
+   * ticking
+   * @param {number} tick
+   * @public
+   */
+  tick(tick = 1) {
+    if (typeof this.callback === "function") {
+      tick = Math.max(1, +tick|0);
+      this.remain -= tick;
+      while (this.remain <= 0) {
+        this.callback();
+        this.remain += this.interval;
       }
-    };
+    }
   }
 }
--- test/tickable-interval.js
+++ test/tickable-interval.js
@@ -15,32 +15,42 @@ describe("TickableInterval", ()=> {
 
     // setInterval
     interval.set(() => { fired += 1 }, 1000);
+    assert(fired === 0);
+    assert(interval.remain === 1000);
 
     // 00:00.000 -> 00:01.000
     interval.tick(250);
     assert(fired === 0);
+    assert(interval.remain === 750);
 
     interval.tick(250);
     assert(fired === 0);
+    assert(interval.remain === 500);
 
     interval.tick(250);
     assert(fired === 0);
+    assert(interval.remain === 250);
 
     interval.tick(250);
     assert(fired === 1);
+    assert(interval.remain === 1000);
 
     // 00:01.000 -> 00:02.000
     interval.tick(250);
     assert(fired === 1);
+    assert(interval.remain === 750);
 
     interval.tick(250);
     assert(fired === 1);
+    assert(interval.remain === 500);
 
     interval.tick(250);
     assert(fired === 1);
+    assert(interval.remain === 250);
 
     interval.tick(250);
     assert(fired === 2);
+    assert(interval.remain === 1000);
 
     // clearInterval
     interval.clear();
@@ -48,27 +58,35 @@ describe("TickableInterval", ()=> {
     // 00:02.000 -> 00:03.000
     interval.tick(250);
     assert(fired === 2);
+    assert(interval.remain === Infinity);
 
     interval.tick(250);
     assert(fired === 2);
+    assert(interval.remain === Infinity);
 
     interval.tick(250);
     assert(fired === 2);
+    assert(interval.remain === Infinity);
 
     interval.tick(250);
     assert(fired === 2);
+    assert(interval.remain === Infinity);
 
     // 00:03.000 -> 00:04.000
     interval.tick(250);
     assert(fired === 2);
+    assert(interval.remain === Infinity);
 
     interval.tick(250);
     assert(fired === 2);
+    assert(interval.remain === Infinity);
 
     interval.tick(250);
     assert(fired === 2);
+    assert(interval.remain === Infinity);
 
     interval.tick(250);
     assert(fired === 2);
+    assert(interval.remain === Infinity);
   });
 });
